75f6090677cd0be2a54ebecc3b3c6333537653ce
Татьяна Гончарова <tanja211030@yandex.ru>
2018-10-03 21:04:12 +0300

    Initial Commit

---
+++ App life cycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App life cycle
+//
+//  Created by Татьяна on 03.10.2018.
+//  Copyright © 2018 Татьяна. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
